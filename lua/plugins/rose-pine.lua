36135d691723425c80549d8dcad902188b2d82c7
Ik-cyber <numterminal@gmail.com>
2025-08-05 13:06:01 +0000

    rose pine

---
+++ lua/plugins/rose-pine.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "main", -- Options: main, moon, dawn (ThePrimeagen likely uses main or moon)
+      dark_variant = "main", -- For dark mode
+      disable_background = false, -- Set to true for transparent backgroundj
+      disable_float_background = true,
+      dim_inactive_windows = false,
+
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+}
